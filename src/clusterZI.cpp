a48fde59b6495fd159d5f6b6ff7af275437a9cd8
Kevin Korsurat <kevin.korsurat@colostate.edu>
2023-07-06 21:31:08 -0600

    update: important variable indicator function
    - Based on Matt's paper
    - Only single iteration

--- src/clusterZI.cpp
+++ src/clusterZI.cpp
@@ -5,11 +5,13 @@
 #define pi 3.141592653589793238462643383280
 
 // Note: -----------------------------------------------------------------------
-// *
-
+// * Cluster index starts with 0.
+// * Update at-risk indicator (done)
+// * Update important variable indicator (done)
+// * Update beta
+// * Update the cluster assignment
 // -----------------------------------------------------------------------------
 
-// User-defined function: ------------------------------------------------------
 // [[Rcpp::export]]
 double log_g_ijk(int j, arma::vec zi, arma::vec gi, arma::vec w, arma::vec xi_k,
                  double b0g, double b1g){
@@ -42,7 +44,76 @@ double log_g_ijk(int j, arma::vec zi, arma::vec gi, arma::vec w, arma::vec xi_k,
 }
 
 // [[Rcpp::export]]
-arma::mat update_gamma(arma::mat z, arma::vec clus_assign, arma::vec w, 
+double log_wj(int j, arma::mat z, arma::mat gamma_mat, arma::vec w, 
+              arma::mat beta, arma::uvec clus_assign, double b0w, double b1w){
+  
+  // Convert beta to xi by taking the exponential function.
+  arma::mat xi = arma::exp(beta);
+  double result = 0.0;
+  
+  // Get the value of wj
+  int wj = w[j];
+  
+  // Calculate the gamma_w_xi factor
+  arma::mat w_mat = arma::repelem(w, 1, z.n_rows); 
+  w_mat = w_mat.t();
+  arma::mat xi_obs = xi.rows(clus_assign);
+  arma::mat gwx = gamma_mat % w_mat % xi_obs;
+  arma::mat gwx_j = gwx.col(j);
+  arma::vec z_gwx_j = gwx_j + z.col(j);
+  
+  result += R::lbeta(b0w + wj, b1w + (1 - wj));
+  result += arma::accu(arma::lgamma(z_gwx_j.elem(arma::find(z_gwx_j != 0))));
+  result -= arma::accu(arma::lgamma(gwx_j.elem(arma::find(gwx_j != 0))));
+  result += arma::accu(arma::lgamma(arma::sum(gwx, 1)));
+  result -= arma::accu(arma::lgamma(arma::sum(z, 1) + arma::sum(gwx, 1)));
+
+  return result;
+  
+} 
+
+// [[Rcpp::export]]
+double log_w(arma::mat z, arma::mat g, arma::vec w, arma::mat xi,
+               arma::uvec clus_assign, double b0w, double b1w){
+  
+  /*
+   * Description: This is a function for calculating probability of the important
+   *              variable indicator for the variable j, p(wj|.), in a log scale.
+   * Input: The index of the current variable (j), the dataset (z), 
+   *        the at-risk indicator for all observation (g), the important 
+   *        variable indicator (w), the xi for all cluster (xi), the 
+   *        hyperparameter of the important variable indicator (b0w, b1w).
+   */
+  
+  double result = 0.0;
+  
+  arma::vec b0 = b0w + w;
+  arma::vec b1 = b1w + (1 - w);
+  
+  result += arma::accu(arma::lgamma(b0) + arma::lgamma(b1) - arma::lgamma(b0 + b1));
+  
+  // Select xi for each observation based on its cluster
+  arma::mat xi_obs = xi.rows(clus_assign);
+  
+  // Duplicate vector w
+  arma::mat w_mat = arma::repelem(w, 1, z.n_rows); 
+  w_mat = w_mat.t();
+  
+  // Calculate gamma_w_xi and z_gamma_w_xi for all observations
+  arma::mat gwx = g % w_mat % xi_obs;
+  arma::mat z_gwx = z + gwx;
+  
+  result += arma::accu(arma::lgamma(arma::sum(gwx, 1)));
+  result -= arma::accu(arma::lgamma(gwx.elem(arma::find(gwx != 0))));
+  result += arma::accu(arma::lgamma(z_gwx.elem(arma::find(z_gwx != 0))));
+  result -= arma::accu(arma::lgamma(arma::sum(z_gwx, 1)));
+  
+  return result;
+  
+}
+
+// [[Rcpp::export]]
+arma::mat update_gamma(arma::mat z, arma::uvec clus_assign, arma::vec w, 
                        arma::mat old_gamma, arma::mat xi, double b0g, double b1g){
   
   /*
@@ -66,7 +137,7 @@ arma::mat update_gamma(arma::mat z, arma::vec clus_assign, arma::vec w,
   for(int i = 0; i < z.n_rows; ++i){
     
     arma::vec zi = arma::conv_to<arma::vec>::from(z.row(i));
-    arma::vec xi_i = arma::conv_to<arma::vec>::from(xi.row(clus_assign[i] - 1));
+    arma::vec xi_i = arma::conv_to<arma::vec>::from(xi.row(clus_assign[i]));
     
     // Consider only the active variables only
     for(int j = 0; j < active_var.size(); ++j){
@@ -105,3 +176,91 @@ arma::mat update_gamma(arma::mat z, arma::vec clus_assign, arma::vec w,
   
   return new_gamma;
 }
+
+// [[Rcpp::export]]
+arma::vec update_w(arma::mat z, arma::uvec clus_assign, arma::vec old_w,
+                   arma::mat gamma_mat, arma::mat beta, double b0w, double b1w){
+  
+  arma::vec proposed_w(old_w);
+  
+  // Get the index of the important and not important variables
+  arma::uvec imp_index = arma::find(proposed_w == 1);
+  arma::uvec n_imp_index = arma::find(proposed_w == 0);
+  int adjusted_index = -1;
+  int j_update = -1;
+  
+  // Propose a new vector of the important variable indicator
+  if(imp_index.size() <= 1){ // If only one variable is active, it cannot delete that one out.
+    adjusted_index = arma::randperm(n_imp_index.size(), 1)[0];
+    j_update = n_imp_index[adjusted_index];
+    proposed_w.row(j_update).fill(1);
+  } else {
+    adjusted_index = arma::randperm(old_w.size(), 1)[0];
+    j_update = adjusted_index;
+    proposed_w.row(j_update).fill(!(old_w[j_update]));
+  }
+  
+  // Calculate the acceptance probability
+  double logA = log_wj(j_update, z, gamma_mat, proposed_w, beta, clus_assign, b0w, b1w) - 
+    log_wj(j_update, z, gamma_mat, old_w, beta, clus_assign, b0w, b1w);
+  double logU = std::log(R::runif(0.0, 1.0));
+  arma::vec new_w(old_w);
+  if(logU <= logA){
+    new_w = proposed_w;
+  }
+  
+  return new_w;
+  
+}
+
+// [[Rcpp::export]]
+arma::mat update_w_test(arma::mat z, arma::uvec clus_assign, arma::vec old_w, 
+                   arma::mat gamma_mat, arma::mat xi, double b0w, double b1w,
+                   int iter_w, double trh){
+  
+  arma::mat result_w(iter_w, old_w.size(), arma::fill::value(-1)); 
+  arma::vec final_w(5, arma::fill::value(-1));
+  
+  arma::vec previous_w(old_w);
+  for(int t = 0; t < iter_w; ++t){
+    
+    arma::vec proposed_w(old_w); 
+    
+    // Get the index of the important and not important variables
+    arma::uvec imp_index = arma::find(previous_w == 1);
+    arma::uvec n_imp_index = arma::find(previous_w == 0);
+    
+    // Decide to perform add/delete or swap
+    if(n_imp_index.size() == 0 or imp_index.size() == 0){
+      arma::uvec adj_var_index = arma::randperm(previous_w.size(), 1);
+      proposed_w.row(adj_var_index[0]).fill(!previous_w[adj_var_index[0]]);
+    } else {
+      double u = R::runif(0, 1);
+      if(u <= 0.5){ // Add/Delete
+        arma::uvec adj_var_index = arma::randperm(previous_w.size(), 1);
+        proposed_w.row(adj_var_index[0]).fill(!previous_w[adj_var_index[0]]);
+      } else { // Swap
+        arma::uvec imp_i = arma::randperm(imp_index.size(), 1);
+        arma::uvec n_imp_i = arma::randperm(n_imp_index.size(), 1);
+        proposed_w.row(imp_index[imp_i[0]]).fill(0);
+        proposed_w.row(n_imp_index[n_imp_i[0]]).fill(1);
+      }
+    }
+    
+    // Calculate the acceptance probability
+    double logA = log_w(z, gamma_mat, proposed_w, xi, clus_assign, b0w, b1w) - 
+      log_w(z, gamma_mat, previous_w, xi, clus_assign, b0w, b1w);
+    double logU = std::log(R::runif(0, 1));
+    if(logU <= logA){
+      previous_w = proposed_w;
+    }
+    
+    result_w.row(t) = proposed_w.t();
+    
+  }
+  
+  
+  
+  return result_w;
+}
+
--- test.R
+++ test.R
@@ -2,7 +2,7 @@ rm(list = ls())
 library(tidyverse)
 
 ### Data Simulation
-data_sim <- function(N, K, J, clus_prop, pi_g, pi_w, s2, lb, ub){
+data_sim <- function(N, K, J, clus_prop, pi_g, pi_w, s2, lb_a, ub_a, lb_ia, ub_ia){
   
   ### cluster assignment
   ci <- sample(1:K, N, replace = TRUE, prob = clus_prop)
@@ -11,36 +11,57 @@ data_sim <- function(N, K, J, clus_prop, pi_g, pi_w, s2, lb, ub){
   gm_mat <- t(apply(matrix(pi_g[ci]), 1, rbinom, n = J, size = 1))
   
   ### w (important variable)
-  w <- rbinom(J, 1, pi_w)
+  w <- sort(rbinom(J, 1, pi_w), decreasing = TRUE)
+  n_active <- sum(w)
   
   ### Generate xi = exp(beta) where beta ~ N(0, s2).
   beta <- matrix(rnorm(J * K, 0, sqrt(s2)), ncol = J)
   xi <- exp(beta)
   
-  ### alpha matrix
-  gm_w <- sweep(gm_mat, MARGIN = 2, w, `*`)
-  w_xi <- sweep(xi[ci, ], MARGIN = 2, w, `*`)
-  alpha_mat <- matrix(rgamma(N * J, gm_w * w_xi, 1), ncol = J)
+  ### Generate the data separately for active and inactive cluster
+  alpha_mat <- matrix(rgamma(K * J, xi, 1), ncol = J)
   
-  ### normalize the alpha matrix
-  alpha_norm <- t(apply(alpha_mat, 1, function(x){x/sum(x)}))
-  
-  ### simulate zi
-  sum_zi <- round(runif(N, lb, ub))
-  zi <- matrix(NA, ncol = J, nrow = N)
+  ### active variable
+  active_alpha <- alpha_mat[ci, (1:n_active)]
+  gm_alpha_active <- gm_mat[, (1:n_active)] * active_alpha
+  alpha_norm_active <- t(apply(gm_alpha_active, 1, function(x){x/sum(x)}))
+  sum_zi_active <- round(runif(N, lb_a, ub_a))
+  zi_active <- matrix(NA, ncol = n_active, nrow = N)
   for(i in 1:N){
-    zi[i, ] <- rmultinom(1, sum_zi[i], alpha_norm[i, ])
+    zi_active[i, ] <- rmultinom(1, sum_zi_active[i], alpha_norm_active[i, ])
   }
+  
+  ### inactive variable
+  inactive_alpha <- alpha_mat[ci, (n_active+1):J]
+  gm_alpha_inactive <- gm_mat[, (n_active+1):J] * inactive_alpha
+  alpha_norm_inactive <- t(apply(gm_alpha_inactive, 1, function(x){x/sum(x)}))
+  sum_zi_inactive <- round(runif(N, lb_ia, ub_ia))
+  zi_inactive <- matrix(NA, ncol = J - n_active, nrow = N)
+  for(i in 1:N){
+    zi_inactive[i, ] <- rmultinom(1, sum_zi_inactive[i], alpha_norm_inactive[i, ])
+  }
+  
+  zi <- cbind(zi_active, zi_inactive)
+  
+  ### Rearrange the variable order
+  variable_order <- sample(1:J, J)
+  gm_mat <- gm_mat[, variable_order]
+  w <- w[variable_order]
+  beta <- beta[, variable_order]
+  zi <- zi[, variable_order]
   
   ### Return the simulated data
   list(ci = ci, gamma = gm_mat, w = w, beta = beta, z = zi)
 }
 
 set.seed(12)
-test_dat <- data_sim(10, 2, 20, c(0.4, 0.6), c(0.4, 0.6), 0.5, 1, 50, 100)
+test_dat <- data_sim(N = 100, K = 3, J = 40, clus_prop = c(0.3, 0.4, 0.3), 
+                     pi_g = c(0.75, 0.9, 0.5), pi_w = 0.65, s2 = 1, lb_a = 500, ub_a = 1000, lb_ia = 250, ub_ia = 350)
 
-new_gamma <- update_gamma(z = test_dat$z, clus_assign = test_dat$ci, w = test_dat$w, 
-                          old_gamma = test_dat$gamma, xi = exp(test_dat$beta), 
-                          b0g = 1, b1g = 1)
 
-cbind(test_dat$z[5, ], test_dat$gamma[5, ], new_gamma[5, ])
+test <- update_w(z = test_dat$z, clus_assign = test_dat$ci - 1, old_w = test_dat$w,
+         gamma_mat = test_dat$gamma, beta = test_dat$beta, b0w = 1, b1w = 1)
+
+table(test, test_dat$w)
+
+identical((test_dat$gamma * (matrix(rep(test_dat$w, 10), nrow = 30) %>% t()) * exp(test_dat$beta[test_dat$ci, ])), test)
