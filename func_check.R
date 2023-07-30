d02e7f70c3f59b757be05ec12ce232335a0acc56
Kevin Korsurat <kevin.korsurat@colostate.edu>
2023-07-29 20:59:04 -0600

    meeting: update code

--- func_check.R
+++ func_check.R
@@ -1,42 +1,56 @@
+library(tidyverse)
+library(reshape2)
+
 ### Import the external function
-source("/Users/kevin-imac/Desktop/Github - Repo/ClusterZI/data/data_sim.R")
+# source("/Users/kevin-imac/Desktop/Github - Repo/ClusterZI/data/data_sim.R")
+source("/Users/kevinkvp/Desktop/Github Repo/ClusterZI/data/data_sim.R")
+
+### Data Simulation
+set.seed(12)
+sim_list <- data_sim(n = 100, K = 3, J_imp = 4, 
+                     pi_gm_mat = matrix(c(0.5), ncol = 10, nrow = 3),
+                     xi_scale = 5, sum_zi_imp = 80, sum_zi_unimp = 20)
+
+### Check
+table(sim_list$ci)
+
+dat <- cbind(index = 1:100, sim_list$z)
+colnames(dat)[2:11] <- str_pad(1:10, 3, pad = "0")
+t2 <- data.frame(dat[which(sim_list$ci == 2), ])
+tt2 <- melt(t2, id.vars = "index")
+tt2$variable <- substr(tt2$variable, 2, 4)
+ggplot(tt2, aes(x = variable, y = value)) + 
+  geom_line(aes(color = index, group = index)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(size = 15), axis.ticks.x = element_blank(),
+        axis.text.y = element_text(size = 15), axis.ticks.y = element_blank(),
+        axis.title.x = element_text(size = 20), axis.title.y = element_text(size = 20),
+        legend.position = "hide", plot.title = element_text(size = 30)) +
+  labs(x = "Variable", y = "")
 
 ### Debugging: at-risk
-#### Generate the data
-#### 50 observations.
-#### 3 clusters with 10 variables (all variables are active).
-
-set.seed(12)
-ci <- sort(rep(1:3, 20))
-beta_mat <- matrix(c(1, 2, 3), nrow = 3, ncol = 10)
-gamma_mat <- matrix(rbinom(10 * 60, 1, 0.5), nrow = 60, ncol = 10)
-alpha_mat <- t(apply(gamma_mat * exp(beta_mat[ci, ]), 1, function(x){x/sum(x)}))
-z <- matrix(NA, ncol = 10, nrow = 60)
-for(i in 1:60){
-  z[i, ] <- rmultinom(1, 10, alpha_mat[i, ])
-}
-
 set.seed(12)
 start_time <- Sys.time()
-test_gamma <- debug_gamma(iter = 10000, K = 3, z = z, clus_assign = ci - 1, 
-                          w = rep(1, 10), beta_mat = beta_mat, r0g = 1, r1g = 1)
+test_gamma <- debug_gamma(iter = 10000, K = 3, z = sim_list$z, clus_assign = sim_list$ci - 1, 
+                          w = c(rep(1, 4), rep(0, 6)), beta_mat = sim_list$beta, r0g = 1, r1g = 1)
 Sys.time() - start_time
 
-gamma_hat <- matrix(NA, ncol = 10, nrow = 60)
-for(i in 1:60){
-  gamma_hat[i, ] <- rowMeans(test_gamma[i, , 5001:1000])
+### The function will update only the first four columns as they are the only 
+### important variables.
+
+gamma_hat <- matrix(NA, ncol = 4, nrow = 100)
+for(i in 1:100){
+  gamma_hat[i, ] <- rowMeans(test_gamma[i, 1:4, 5001:1000])
 }
 
-result_0726 <- list(z = z, gamma_mat = gamma_mat, gamma_hat = gamma_hat)
-saveRDS(result_0726,
-        file = "/Users/kevin-imac/Desktop/Github - Repo/ClusterZI/result_0726.rds")
-result_0726$gamma_mat
+gamma_hat[which(sim_list$ci == 1), ][1:4, ]
+sim_list$gamma[which(sim_list$ci == 1), 1:4][1:4, ]
+sim_list$z[which(sim_list$ci == 1), 1:4][1:4, ]
 
-mean(result_0726$gamma_hat[2, result_0726$gamma_hat[2, ] != 1])
-sd(result_0726$gamma_hat[2, result_0726$gamma_hat[2, ] != 1])
+sim_list$z[, 1]
+sim_list$z[, 3]
 
-mean(result_0726$gamma_hat[3, result_0726$gamma_hat[3, ] != 1])
-sd(result_0726$gamma_hat[3, result_0726$gamma_hat[3, ] != 1])
-
-
-gamma_hat[gamma_hat < 0.5]
+sim_list$z[1, 1:4]
+sim_list$ci[1]
+sim_list$gamma[1, 1:4] * exp(sim_list$beta[2, 1:4])
+sim_list$z[1, 1:4] + (sim_list$gamma[1, 1:4] * exp(sim_list$beta[2, 1:4]))
