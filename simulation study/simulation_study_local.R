f79a3406ca5a67b941a0ebe2368f52807dd1ca06
Kevin Korsurat <kevin.korsurat@colostate.edu>
2023-09-12 22:06:08 -0600

    update file for running locally

--- simulation study/simulation_study_local.R
+++ simulation study/simulation_study_local.R
@@ -4,19 +4,15 @@ n_core <- 5
 dat_rep <- 30
 
 base_path <- "/Users/kevin-imac/Desktop/"
-case_name <- "strong_sig_with_overlap_zi"
+case_name <- "weak_sig_with_overlap_zi"
 
 #### Data Hyperparameters
 xi_c <- 100
-pig <- 0.85
+pig <- 0.65
 
 #### Model Hyperparameters
 MH_v <- 1
 burn_in <- 5000
-
-#### Check
-##### () Normal: pi_gm = 0.95 and x_conc = 10
-##### () ZI: pi_gm = 0.85 and xi_conc = 100
 
 ###: ---------------------------------------------------------------------------
 
@@ -32,15 +28,12 @@ source(paste0(base_path, "Github - Repo/ClusterZI/data/data_sim_DM.R"))
 registerDoParallel(n_core)
 data_sim <- foreach(t = 1:dat_rep) %dopar% {
   set.seed(t)
-  pat_mat <- diag(20)[1:5, ]
-  pat_mat[3, 3] <- 0
-  pat_mat[4, 4] <- 0
-  pat_mat[5, 5] <- 0
-  pat_mat[3, c(10, 12)] <- 1
-  pat_mat[4, c(11, 12)] <- 1
-  pat_mat[5, c(10, 11)] <- 1
+  pat_mat <- matrix(0, nrow = 3, ncol = 20)
+  pat_mat[1, 1:10] <- 1
+  pat_mat[2, 8:15] <- 1
+  pat_mat[3, 15:20] <- 1
   sim_dat <- simDM(n = 200, pattern = pat_mat, xi_conc = xi_c, pi_gm = c(pig), 
-                   pi_c = c(1, 1, 1, 1, 1), z_sum_L = 500, z_sum_U = 1000, 
+                   pi_c = c(1, 1, 1), z_sum_L = 500, z_sum_U = 1000, 
                    theta = 0.01)
   sim_dat_sum <- simDM_sum(sim_dat)$summary_zero
   return(list(data = sim_dat, zero = sim_dat_sum))
@@ -96,7 +89,7 @@ shi_result <- foreach(t = 1:dat_rep) %dopar% {
   
   start_time <- Sys.time()
   shi_mod <- PerformClustering(t(data_sim[[t]]$data$z), "DP", w = 1, 
-                               beta1 = 1000, beta2 = 1, totaliter = 10000, 
+                               beta1 = 1, beta2 = 1, totaliter = 10000, 
                                burnin = 5000, thin = 1)
   shi_time <- difftime(Sys.time(), start_time, units = "secs")
 
@@ -129,3 +122,34 @@ saveRDS(list(dat = data_sim), paste0(base_path, "simu_result/data_", case_name, 
 saveRDS(list(zz_result = zz_result, dz_result = dz_result,
              dd_result = dd_result, shi_result = shi_result), 
         paste0(base_path, "simu_result/result_", case_name, ".RData"))
+
+### Fix Shi's Model: -----------------------------------------------------------
+#### First, Import the data
+base_path <- "/Users/kevin-imac/Desktop/"
+case_name <- "strong_sig_with_overlap_zi"
+n_core = 5
+dat_rep = 30
+dat <- readRDS(paste0(base_path, "simu_result/data_", case_name, ".RData"))
+
+start_shi_fix <- Sys.time()
+registerDoParallel(n_core)
+shi_result <- foreach(t = 1:dat_rep) %dopar% {
+  
+  start_time <- Sys.time()
+  shi_mod <- PerformClustering(t(dat$dat[[t]]$data$z), "DP", w = 1, 
+                               beta1 = 1, beta2 = 1, totaliter = 10000, 
+                               burnin = 5000, thin = 1)
+  shi_time <- difftime(Sys.time(), start_time, units = "secs")
+  
+  return(list(shi_mod = shi_mod, shi_time = shi_time))
+  
+}
+stopImplicitCluster()
+Sys.time() - start_shi_fix
+
+saveRDS(list(fix_shi_result = shi_result), 
+        paste0(base_path, "simu_result/fix_shi_result_", case_name, ".RData"))
+
+rm(list = ls())
+
+
