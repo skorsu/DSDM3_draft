3ddeac2bd03141d7ba8e100ff8b440cc85a602f3
Kevin Korsurat <kevin.korsurat@colostate.edu>
2024-01-27 22:34:01 -0700

    Update sim_study_multi_clus.R

--- simulation study/sim_study_multi_clus.R
+++ simulation study/sim_study_multi_clus.R
@@ -133,7 +133,7 @@ stopImplicitCluster()
 ### Save the simulated data
 save_path <- "/Users/kevinkvp/Desktop/Github Repo/ClusterZI/simulation study/result_1222/"
 # save_path <- "/Users/kevin-imac/Desktop/"
-case_name <- "diffindex_3_K_5"
+case_name <- "diffindex3_K_5"
 saveRDS(datSimK, paste0(save_path, case_name, "_simDat.RData"))
 
 ### Example of the data --------------------------------------------------------
@@ -174,6 +174,71 @@ stopImplicitCluster()
 difftime(Sys.time(), start_ova)
 saveRDS(resultZZ, paste0(save_path, case_name, "_ZZ.RData"))
 
+### DM-ZIDM
+start_ova <- Sys.time()
+registerDoParallel(5)
+resultDZ <- foreach(t = 1:nData) %dopar% {
+  
+  start_time <- Sys.time()
+  clus_result <- DMZIDM(iter = 100000, Kmax = 10, z = dat[[t]]$dat, 
+                        beta_mat = matrix(0, ncol = 50, nrow = 10), 
+                        ci_init = rep(0, 100), 
+                        theta = 1, launch_iter = 10, r0c = 1, r1c = 1, 
+                        thin = 100)
+  tot_time <- difftime(Sys.time(), start_time, units = "secs")
+  list(time = tot_time, result = clus_result)
+  
+}
+stopImplicitCluster()
+difftime(Sys.time(), start_ova)
+
+saveRDS(resultDZ, paste0(save_path, case_name, "_DZ.RData"))
+
+### DM-DM
+start_ova <- Sys.time()
+registerDoParallel(5)
+resultDD <- foreach(t = 1:nData) %:%
+  foreach(k = 2:10) %dopar% {
+    start_time <- Sys.time()
+    clus_result <- DMDM(iter = 100000, Kmax = k, z = dat[[t]]$dat, 
+                        beta_mat = matrix(0, ncol = 50, nrow = k), 
+                        ci_init = rep(0, 100), 
+                        theta = 1, thin = 100)
+    tot_time <- difftime(Sys.time(), start_time, units = "secs")
+    list(time = tot_time, result = clus_result)
+  }
+stopImplicitCluster()
+difftime(Sys.time(), start_ova)
+
+saveRDS(resultDD, paste0(save_path, case_name, "_DD.RData"))
+
+### DM-sDM
+start_ova <- Sys.time()
+registerDoParallel(5)
+resultDsD <- foreach(t = 1:nData) %dopar% {
+  
+  start_time <- Sys.time()
+  clus_result <- DMDM(iter = 100000, Kmax = 10, z = dat[[t]]$dat, 
+                      beta_mat = matrix(0, ncol = 50, nrow = 10), 
+                      ci_init = rep(0, 100), 
+                      theta = 1e-10, thin = 100)
+  tot_time <- difftime(Sys.time(), start_time, units = "secs")
+  list(time = tot_time, result = clus_result)
+  
+}
+stopImplicitCluster()
+difftime(Sys.time(), start_ova)
+
+saveRDS(resultDsD, paste0(save_path, case_name, "_DsD.RData"))
+
+### Begin the analysis ---------------------------------------------------------
+
+
+
+
+
+
+
 sapply(1:nData, function(x){resultZZ[[x]]$time}) %>% meanSD()
 
 sapply(1:nData, 
@@ -207,85 +272,6 @@ sapply(1:nData,
 
 
 
-
-
-
-### DM-ZIDM
-start_ova <- Sys.time()
-registerDoParallel(5)
-resultDZ <- foreach(t = 1:nData) %dopar% {
-  
-  start_time <- Sys.time()
-  clus_result <- DMZIDM(iter = 100000, Kmax = 10, z = dat$dat[[t]], 
-                        beta_mat = matrix(0, ncol = 50, nrow = 10), 
-                        ci_init = rep(0, 50), 
-                        theta = 10, launch_iter = 10, r0c = 1, r1c = 1, 
-                        thin = 100)
-  tot_time <- difftime(Sys.time(), start_time, units = "secs")
-  list(time = tot_time, result = clus_result)
-  
-}
-stopImplicitCluster()
-difftime(Sys.time(), start_ova)
-
-saveRDS(resultDZ, paste0(save_path, case_name, "_DZ.RData"))
-
-# start_ova <- Sys.time()
-# registerDoParallel(5)
-# resultDZ <- foreach(t = 1:nData) %:%
-#   foreach(k = 2:10) %dopar% {
-#     start_time <- Sys.time()
-#     clus_result <- DMZIDM(iter = 100000, Kmax = k, z = dat$dat[[t]], 
-#                           beta_mat = matrix(0, ncol = 50, nrow = 10), 
-#                           ci_init = rep(0, 50), 
-#                           theta = 10, launch_iter = 10, r0c = 1, r1c = 1, 
-#                           thin = 100)
-#     tot_time <- difftime(Sys.time(), start_time, units = "secs")
-#     list(time = tot_time, result = clus_result)
-#   }
-# stopImplicitCluster()
-# difftime(Sys.time(), start_ova)
-# 
-# saveRDS(resultDZ, paste0(save_path, case_name, "_DZ.RData"))
-
-### DM-DM
-start_ova <- Sys.time()
-registerDoParallel(5)
-resultDD <- foreach(t = 1:nData) %:%
-  foreach(k = 2:10) %dopar% {
-  start_time <- Sys.time()
-  clus_result <- DMDM(iter = 100000, Kmax = k, z = dat$dat[[t]], 
-                      beta_mat = matrix(0, ncol = 50, nrow = 10), 
-                      ci_init = rep(0, 50), 
-                      theta = 10, thin = 100)
-  tot_time <- difftime(Sys.time(), start_time, units = "secs")
-  list(time = tot_time, result = clus_result)
-}
-stopImplicitCluster()
-difftime(Sys.time(), start_ova)
-
-saveRDS(resultDD, paste0(save_path, case_name, "_DD.RData"))
-
-### DM-sDM
-start_ova <- Sys.time()
-registerDoParallel(5)
-resultDsD <- foreach(t = 1:nData) %dopar% {
-  
-  start_time <- Sys.time()
-  clus_result <- DMDM(iter = 100000, Kmax = 10, z = dat$dat[[t]], 
-                      beta_mat = matrix(0, ncol = 50, nrow = 10), 
-                      ci_init = rep(0, 50), 
-                      theta = 1e-10, thin = 100)
-  tot_time <- difftime(Sys.time(), start_time, units = "secs")
-  list(time = tot_time, result = clus_result)
-  
-}
-stopImplicitCluster()
-difftime(Sys.time(), start_ova)
-
-saveRDS(resultDsD, paste0(save_path, case_name, "_DsD.RData"))
-
-### Begin the analysis ---------------------------------------------------------
 
 #### (If needed) Load the result -----------------------------------------------
 # save_path <- "/Users/kevinkvp/Desktop/Github Repo/ClusterZI/simulation study/result_1224/"
