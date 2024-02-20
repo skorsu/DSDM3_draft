40a49f5527c50e67eeda1cd12efc2c3010fe4ae1
Kevin Korsurat <kevin.korsurat@colostate.edu>
2024-02-19 21:10:09 -0700

    update the code for running the DTMM on goose

--- Manuscript/Code/simu_model_compete_goose.R
+++ Manuscript/Code/simu_model_compete_goose.R
@@ -2,15 +2,14 @@
 library(tidyverse)
 library(foreach)
 library(doParallel)
-library(sparseMbClust)
-library(ecodist)
 library(ape)
 library(DTMM)
 library(salso)
 
 ### Change the settings --------------------------------------------------------
 case_name <- "diffindex_3_K_5"
-nData <- 20
+nCore <- 5
+nData <- 5
 
 ### Import the data ------------------------------------------------------------
 path <- "/Users/kevin-imac/Desktop/Github - Repo/"
@@ -19,6 +18,7 @@ if(! file.exists(path)){
 }
 
 dat <- readRDS(paste0(path, "ClusterZI/Manuscript/Data/", case_name, "_simDat.RData"))
+
 dat_J <- ncol(dat[[1]]$dat) 
 dat_n <- nrow(dat[[1]]$dat) 
 
@@ -43,35 +43,48 @@ adjDTMM <- function(Y, tree, tau_vec = 10 ^ seq(-1, 4, 0.5), nu_vec = 1,
           init_c, gamma_sample, alpha, mcmc_iter, select = FALSE)
 }
 
-
 ### Run the models -------------------------------------------------------------
 #### DTMM: with no structures
 set.seed(3124, kind = "L'Ecuyer-CMRG")
 start_ova <- Sys.time()
-registerDoParallel(5)
+registerDoParallel(nCore)
 resultDTMM <- foreach(t = 1:nData) %dopar% {
   
   start_time <- Sys.time()
   clus_result <- adjDTMM(dat[[t]]$dat, 
-                         tree = read.tree(text = paste("(", paste(1:50, collapse = ", "), ");")), 
+                         tree = read.tree(text = paste("(", paste(1:dat_J, collapse = ", "), ");")), 
                          init_c = rep(1, 100), mcmc_iter = 100000)
   tot_time <- difftime(Sys.time(), start_time, units = "secs")
-  list(time = tot_time, result = clus_result$post_c[, (1:1000) * 100])
+  list(time = tot_time, result = t(clus_result$post_c[, (1:1000) * 100]))
   
 }
 stopImplicitCluster()
 difftime(Sys.time(), start_ova)
 saveRDS(resultDTMM, paste0(save_path, case_name, "_DTMM_no_structure.RData"))
 
-################################################################################
+#### DTMM: with structures
+set.seed(3124, kind = "L'Ecuyer-CMRG")
+start_ova <- Sys.time()
+registerDoParallel(5)
+resultDTMM <- foreach(t = 1:nData) %dopar% {
+  
+  tre <- nj(dist(t(dat[[t]]$dat)))
+  tre <- ladderize(tre)
+  
+  start_time <- Sys.time()
+  clus_result <- adjDTMM(dat[[t]]$dat, 
+                         tree = tre, 
+                         init_c = rep(1, 100), mcmc_iter = 100000)
+  tot_time <- difftime(Sys.time(), start_time, units = "secs")
+  list(time = tot_time, result = t(clus_result$post_c[, (1:1000) * 100]),
+       tree = tre)
+  
+}
+stopImplicitCluster()
+difftime(Sys.time(), start_ova)
+saveRDS(resultDTMM, paste0(save_path, case_name, "_DTMM_with_structure.RData"))
 
-### Reconstruct the phylogenetic trees based on the dataset
-### https://fuzzyatelin.github.io/bioanth-stats/module-24/module-24.html#objectives
-tre <- nj(dist(t(dat[[1]]$dat)))
-tre <- ladderize(tre)
-plot(tre, cex = 0.6)
-plot(as.vector(dist(t(dat[[1]]$dat))), as.vector(as.dist(cophenetic(tre))))
-cor(as.vector(dist(t(dat[[1]]$dat))), as.vector(as.dist(cophenetic(tre))))^2
+################################################################################
 
 
 
