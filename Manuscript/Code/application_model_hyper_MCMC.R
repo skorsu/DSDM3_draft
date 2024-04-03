e3b35bb908956178fa8d09174e08383d95bdc665
Kevin Korsurat <kevin.korsurat@colostate.edu>
2024-04-02 23:36:26 -0600

    update MCMC: 6-month

---
+++ Manuscript/Code/application_model_hyper_MCMC.R
@@ -0,0 +1,130 @@
+### Load libraries -------------------------------------------------------------
+library(Rcpp)
+library(foreach)
+library(doParallel)
+library(stringr)
+library(tidyverse)
+library(salso)
+library(ggplot2)
+
+### Import the data ------------------------------------------------------------
+path <- "/Users/kevin-imac/Desktop/Github - Repo/ClusterZI/"
+if(! file.exists(path)){
+  path <- "/Users/kevinkvp/Desktop/Github Repo/ClusterZI/"
+}
+
+datpath <- "/Users/kevin-imac/Desktop/Annika/"
+if(! file.exists(datpath)){
+  datpath <- "/Users/kevinkvp/Desktop/Annika/Application/"
+}
+
+### Data: 6 and 8 Months
+ni68 <- read.csv(paste0(datpath, "Data/Nicaragua_6mo_8mo_genus.csv"))
+ml68 <- read.csv(paste0(datpath, "Data/Mali_6mo_8mo_genus.csv"))
+
+### Data: 12 Months
+ni12 <- read.csv(paste0(datpath, "Data/Nicaragua_12mo_Metadata_csv.csv"))
+ml12 <- read.csv(paste0(datpath, "Data/Mali_12mo_Metadata_csv.csv"))
+
+### Result
+result6mo <- readRDS(paste0(path, "Manuscript/Result/microbiome_result_6m.RData"))
+# result01 <- readRDS(paste0(path, "Manuscript/Result/microbiome_result_6m_oneClus.RData"))
+# result90 <- readRDS(paste0(path, "Manuscript/Result/microbiome_result_6m_singleton.RData"))
+# result30 <- readRDS(paste0(path, "Manuscript/Result/microbiome_result_6m_init30.RData"))
+# result60 <- readRDS(paste0(path, "Manuscript/Result/microbiome_result_6m_init60.RData"))
+
+### User-defined Functions -----------------------------------------------------
+meanSD <- function(x, dplace = 3){
+  mm <- round(mean(x), digits = dplace)
+  ss <- round(sd(x), digits = dplace)
+  paste0(mm, " (", ss, ")")
+}
+
+uniqueClus <- function(x){
+  length(unique(x))
+}
+
+### MCMC: Convergence ----------------------------------------------------------
+lapply(1:8, function(x){data.frame(Cluster = apply(result6mo[[x]]$MCMC$result$ci_result, 1, uniqueClus),
+                                   Chain = paste0(result6mo[[x]]$init, ": Chain ", (x%%2) + 1),
+                                   Iteration = 1:10000)}) %>%
+  bind_rows(.id = NULL) %>%
+  ggplot(aes(x = Iteration, y = Cluster, color = Chain)) +
+  geom_line() +
+  theme_bw() +
+  scale_y_continuous(name = "Active Clusters", limits = c(1, 10), breaks = seq(1, 10)) +
+  labs(title = "6 Month: Active Clusters via MCMC with different initial cluster assignment") +
+  theme(legend.position = "bottom", legend.box = "horizontal")
+
+sapply(1:8, function(x){apply(result6mo[[x]]$MCMC$result$ci_result, 1, uniqueClus)})
+
+### salso: combine all chains --------------------------------------------------
+result6mo[[1]]$MCMC$result$ci_result[seq(5000, 10000, 10), ]
+rbind(result6mo[[1]]$MCMC$result$ci_result[seq(5000, 10000, 100), ],
+      result6mo[[2]]$MCMC$result$ci_result[seq(5000, 10000, 100), ],
+      result6mo[[3]]$MCMC$result$ci_result[seq(5000, 10000, 100), ],
+      result6mo[[4]]$MCMC$result$ci_result[seq(5000, 10000, 100), ],
+      result6mo[[5]]$MCMC$result$ci_result[seq(5000, 10000, 100), ],
+      result6mo[[6]]$MCMC$result$ci_result[seq(5000, 10000, 100), ],
+      result6mo[[7]]$MCMC$result$ci_result[seq(5000, 10000, 100), ],
+      result6mo[[8]]$MCMC$result$ci_result[seq(5000, 10000, 100), ]) %>%
+  salso()
+
+
+# result6mo <- vector("list", length = 8)
+# result6mo[[1]] <- list(MCMC = result01[[5]], init = "One Cluster")
+# result6mo[[2]] <- list(MCMC = result01[[6]], init = "One Cluster")
+# result6mo[[3]] <- list(MCMC = result90[[4]], init = "Singleton")
+# result6mo[[4]] <- list(MCMC = result90[[6]], init = "Singleton")
+# result6mo[[5]] <- list(MCMC = result30[[5]], init = "30 Clusters")
+# result6mo[[6]] <- list(MCMC = result30[[7]], init = "30 Clusters")
+# result6mo[[7]] <- list(MCMC = result60[[6]], init = "60 Clusters")
+# result6mo[[8]] <- list(MCMC = result60[[7]], init = "60 Clusters")
+# saveRDS(result6mo, paste0(path, "Manuscript/Result/microbiome_result_6m.RData")) 
+# 
+# 
+# 
+# 
+# sapply(1:7, function(x){apply(result01[[x]]$result$ci_result, 1, uniqueClus)})[10000, ]
+# sapply(c(5, 6), function(x){apply(result01[[x]]$result$ci_result, 1, uniqueClus)}) %>%
+#   as.data.frame() %>%
+#   mutate(Iteration = 1:10000) %>%
+#   pivot_longer(!c(Iteration), names_to = "Chain", values_to = "Cluster") %>%
+#   ggplot(aes(x = Iteration, y = Cluster, color = Chain)) +
+#   geom_line() +
+#   theme_bw() +
+#   labs(title = "One Cluster: nB = 1, r0g = 4, s2 = 1")
+# 
+# sapply(1:7, function(x){apply(result90[[x]]$result$ci_result, 1, uniqueClus)})[10000, ]
+# sapply(c(4, 6), function(x){apply(result90[[x]]$result$ci_result, 1, uniqueClus)}) %>%
+#   as.data.frame() %>%
+#   mutate(Iteration = 1:10000) %>%
+#   pivot_longer(!c(Iteration), names_to = "Chain", values_to = "Cluster") %>%
+#   ggplot(aes(x = Iteration, y = Cluster, color = Chain)) +
+#   geom_line() +
+#   theme_bw() +
+#   labs(title = "One Cluster: nB = 1, r0g = 4, s2 = 1")
+# 
+# sapply(1:7, function(x){apply(result30[[x]]$result$ci_result, 1, uniqueClus)})[10000, ]
+# sapply(c(5, 7), function(x){apply(result30[[x]]$result$ci_result, 1, uniqueClus)}) %>%
+#   as.data.frame() %>%
+#   mutate(Iteration = 1:10000) %>%
+#   pivot_longer(!c(Iteration), names_to = "Chain", values_to = "Cluster") %>%
+#   ggplot(aes(x = Iteration, y = Cluster, color = Chain)) +
+#   geom_line() +
+#   theme_bw() +
+#   labs(title = "One Cluster: nB = 1, r0g = 4, s2 = 1")
+# 
+# sapply(1:7, function(x){apply(result60[[x]]$result$ci_result, 1, uniqueClus)})[10000, ]
+# sapply(6:7, function(x){apply(result60[[x]]$result$ci_result, 1, uniqueClus)}) %>%
+#   as.data.frame() %>%
+#   mutate(Iteration = 1:10000) %>%
+#   pivot_longer(!c(Iteration), names_to = "Chain", values_to = "Cluster") %>%
+#   ggplot(aes(x = Iteration, y = Cluster, color = Chain)) +
+#   geom_line() +
+#   theme_bw() +
+#   labs(title = "One Cluster: nB = 1, r0g = 4, s2 = 1")
+
+
+
+
