23771a6364e616b21c8615c860888dd21380059a
Kevin Korsurat <kevin.korsurat@colostate.edu>
2024-04-11 14:37:12 -0600

    Update application_model_AD_analysis.R

--- Manuscript/Code/application_model_AD_analysis.R
+++ Manuscript/Code/application_model_AD_analysis.R
@@ -8,6 +8,8 @@ library(ggplot2)
 library(readxl)
 library(latex2exp)
 library(MCMCprecision)
+library(gridExtra)
+library(ggpubr)
 
 ### User-defined Functions: ----------------------------------------------------
 meanSD <- function(x, dplace = 3){
@@ -142,16 +144,288 @@ addDat <- rbind(addml %>%
 ### Run the salso: -------------------------------------------------------------
 salsoClus <- sapply(1:3, function(x){salso(listData[[x]]$ci_result)})
 
+### Actual relative abundance for each observation: ----------------------------
+actRela <- lapply(list(dat06[, -(1:5)], dat08[, -(1:5)], dat12[, -(1:5)]),
+                  function(x){as.matrix(x/rowSums(x))})
+
 ### Estimate the relative abundance for each observation: ----------------------
 set.seed(1)
-ci <- listData[[1]]$ci_result[, 1] + 1
-betaVec <- t(sapply(1:2000, function(x){listData[[1]]$beta_result[[x]][ci[x], ]}))
-expBetaAR <- exp(betaVec) * t(listData[[1]]$atrisk_result[1, , ])
+estRela <- lapply(1:3, function(z){sapply(1:90, function(y){ci <- listData[[z]]$ci_result[, y] + 1
+betaVec <- t(sapply(1:2000, function(x){listData[[z]]$beta_result[[x]][ci[x], ]}))
+expBetaAR <- exp(betaVec) * t(listData[[z]]$atrisk_result[y, , ])
 simRelaAbun <- t(sapply(1:2000, function(x){as.numeric(rdirichlet(1, expBetaAR[x, ]))}))
-cbind(colMeans(simRelaAbun), as.numeric(dat06[1, -(1:5)])/sum(as.numeric(dat06[1, -(1:5)])))
+colMeans(simRelaAbun)}) %>% 
+    t()})
+
+### Sort by some bacteria: -----------------------------------------------------
+### Group Taxa
+datList <- list(dat06, dat08, dat12)
+
+registerDoParallel(5)
+impTaxa <- foreach(t = 1:3) %dopar% {
+  
+  prop <- as.numeric(colMeans(datList[[t]][, -(1:5)]/rowSums(datList[[t]][, -(1:5)])))
+  which(prop %in% boxplot.stats(prop)$out)
+  
+}
+stopImplicitCluster()
+impTaxa <- union(impTaxa[[1]], union(impTaxa[[2]], impTaxa[[3]]))
+
+taxaName <- cbind(str_match(colnames(dat06[-(1:5)]), "p\\_{2}\\.?[:alpha:]+") %>%
+                    str_match("[:upper:]{1}[:alpha:]+"), 
+                  str_match(colnames(dat06[-(1:5)]), "g\\_{2}\\.?[:alpha:]+") %>%
+                    str_match("[:upper:]{1}[:alpha:]+"))
+
+impTaxaInd <- rep(FALSE, 38)
+impTaxaInd[impTaxa] <- TRUE
+
+#### Get the column name, group the other taxa
+showName <- data.frame(taxaName, impTaxaInd) %>%
+  replace_na(list(X1 = "Bacteria", X2 = "Other")) %>%
+  mutate(showName = ifelse(impTaxaInd, X2, 
+                           ifelse(X1 == "Bacteria", "Others", paste0("Other ", X1)))) %>%
+  .$showName
+
+showName <- factor(showName,
+                   levels = c("Bifidobacterium", "Other Actinobacteria",
+                              "Bacteroides", "Prevotella", "Other Bacteroidetes",
+                              "Faecalibacterium", "Megasphaera", "Ruminococcus", 
+                              "Streptococcus", "Veillonella", "Other Firmicutes", 
+                              "Others"),
+                   labels = c("Bifidobacterium", "Other_Actinobacteria",
+                              "Bacteroides", "Prevotella", "Other_Bacteroidetes",
+                              "Faecalibacterium", "Megasphaera", "Ruminococcus", 
+                              "Streptococcus", "Veillonella", "Other_Firmicutes", 
+                              "Others"))
+
+obsID <- c(str_replace_all(str_extract(dat08$ID[1:45], "^([:alpha:]{2}\\.){2}[:digit:]{2}"), "\\.", "\\-"),
+           str_extract(dat08$ID[-(1:45)], "^[:digit:]{7}"))
+bactList <- c("Bifidobacterium", "Other_Actinobacteria", "Bacteroides", 
+              "Prevotella", "Other_Bacteroidetes", "Faecalibacterium", 
+              "Megasphaera", "Ruminococcus", "Streptococcus", "Veillonella", 
+              "Other_Firmicutes", "Others")
+colours <- c("#A4C639", "#BAC394", "#DDA661", "#BD8B46", "#FFD197",
+             "#BC6D62", "#DE6A5D", "#DE998B", "#FF6558", "#FFC6B8", 
+             "#FFE4DA", "#E5E5E5")
+
+### Group the bacteria
+groupRelaTaxa <- lapply(1:3, function(x){
+  
+  estiRelaGroup <- matrix(NA, nrow = 90, ncol = 12)
+  actualRelaGroup <- matrix(NA, nrow = 90, ncol = 12)
+  
+  for(i in 1:length(bactList)){
+    index <- which(showName == bactList[i])
+    if(length(index) == 1){
+      estiRelaGroup[, i] <- estRela[[x]][, which(showName == bactList[i])]
+      actualRelaGroup[, i] <- actRela[[x]][, which(showName == bactList[i])]
+    } else {
+      estiRelaGroup[, i] <- rowSums(estRela[[x]][, which(showName == bactList[i])])
+      actualRelaGroup[, i] <- rowSums(actRela[[x]][, which(showName == bactList[i])])
+    }
+  }
+  
+  colnames(estiRelaGroup) <- bactList
+  colnames(actualRelaGroup) <- bactList
+  
+  list(actualRelaGroup = actualRelaGroup, estiRelaGroup = estiRelaGroup)
+  
+})
+
+### Rearrange the cluster assignment: ------------------------------------------
+# colnames(groupRelaTaxa[[1]]$actualRelaGroup)
+# [1] "Bifidobacterium"     
+# [2] "Other_Actinobacteria"
+# [3] "Bacteroides"         
+# [4] "Prevotella"          
+# [5] "Other_Bacteroidetes" 
+# [6] "Faecalibacterium"    
+# [7] "Megasphaera"         
+# [8] "Ruminococcus"        
+# [9] "Streptococcus"       
+# [10] "Veillonella"         
+# [11] "Other_Firmicutes"    
+# [12] "Others"  
+
+orderedClus <- sapply(1:3, function(x){
+  newIndex <- cbind(y = groupRelaTaxa[[x]]$estiRelaGroup[, "Bifidobacterium"], 
+                    clus = salsoClus[, x]) %>%
+    as.data.frame() %>%
+    group_by(clus) %>%
+    summarise(mean = mean(y)) %>%
+    .$mean %>%
+    sort(decreasing = TRUE, index.return=TRUE) %>%
+    .$ix
+  newCLUS <- rep(NA, 90)
+  for(i in 1:2){
+    newCLUS[which(salsoClus[, x] == newIndex[i])] <- i
+  }
+  
+  newCLUS
+  
+})
+
+### Relative Plots: ------------------------------------------------------------
+relaPlot <- function(group_relaAbunList, clus_assign, actual_month){
+  
+  ### Actual
+  actual_title <- paste0(actual_month, " Infants: Actual Relative Abundance")
+  actPlot <- data.frame(group_relaAbunList$actualRelaGroup) %>%
+    mutate(id = obsID, Cluster = paste0("Cluster ", clus_assign)) %>%
+    pivot_longer(!c(id, Cluster), names_to = "Bacteria", values_to = "Prop") %>%
+    ggplot(aes(fill = factor(Bacteria, levels = c("Bifidobacterium", "Other_Actinobacteria",
+                                                  "Bacteroides", "Prevotella", "Other_Bacteroidetes",
+                                                  "Faecalibacterium", "Megasphaera", "Ruminococcus", 
+                                                  "Streptococcus", "Veillonella", "Other_Firmicutes", 
+                                                  "Others"),
+                             labels = c("Bifidobacterium", "Other Actinobacteria",
+                                        "Bacteroides", "Prevotella", "Other Bacteroidetes",
+                                        "Faecalibacterium", "Megasphaera", "Ruminococcus", 
+                                        "Streptococcus", "Veillonella", "Other Firmicutes", 
+                                        "Others")), y = Prop, x = id)) + 
+    geom_bar(position="fill", stat = "identity") +
+    scale_fill_manual("", values = colours) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, size = 5.5), legend.position = "bottom") +
+    guides(fill = guide_legend(nrow = 1, byrow = TRUE)) +
+    scale_y_continuous(labels = scales::percent_format()) +
+    labs(y = "Relative Abundance", title = actual_title, x = "Sample") +
+    facet_grid(~ Cluster, scales = "free_x")
+  
+  ### Estimate
+  estimate_title <- paste0(actual_month, " Infants: Estimated Relative Abundance")
+  estPlot <- data.frame(group_relaAbunList$estiRelaGroup) %>%
+    mutate(id = obsID, Cluster = paste0("Cluster ", clus_assign)) %>%
+    pivot_longer(!c(id, Cluster), names_to = "Bacteria", values_to = "Prop") %>%
+    ggplot(aes(fill = factor(Bacteria, levels = c("Bifidobacterium", "Other_Actinobacteria",
+                                                  "Bacteroides", "Prevotella", "Other_Bacteroidetes",
+                                                  "Faecalibacterium", "Megasphaera", "Ruminococcus", 
+                                                  "Streptococcus", "Veillonella", "Other_Firmicutes", 
+                                                  "Others"),
+                             labels = c("Bifidobacterium", "Other Actinobacteria",
+                                        "Bacteroides", "Prevotella", "Other Bacteroidetes",
+                                        "Faecalibacterium", "Megasphaera", "Ruminococcus", 
+                                        "Streptococcus", "Veillonella", "Other Firmicutes", 
+                                        "Others")), y = Prop, x = id)) + 
+    geom_bar(position="fill", stat = "identity") +
+    scale_fill_manual("", values = colours) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, size = 5.5), legend.position = "bottom") +
+    guides(fill = guide_legend(nrow = 1, byrow = TRUE)) +
+    scale_y_continuous(labels = scales::percent_format()) +
+    labs(y = "Estimated Relative Abundance", title = estimate_title, x = "Sample") +
+    facet_grid(~ Cluster, scales = "free_x")
+  
+  list(actPlot = actPlot, estPlot = estPlot)
+  
+}
+
+abr6m <- relaPlot(groupRelaTaxa[[1]], orderedClus[, 1], "6-Month")
+abr8m <- relaPlot(groupRelaTaxa[[2]], orderedClus[, 2], "8-Month")
+abr12m <- relaPlot(groupRelaTaxa[[3]], orderedClus[, 3], "12-Month")
+
+ggarrange(abr6m$actPlot, abr8m$actPlot, abr12m$actPlot,
+          abr6m$estPlot, abr8m$estPlot, abr12m$estPlot,
+          ncol = 3, nrow = 2, common.legend = TRUE, legend = "bottom")
+
+
+### Descriptive of the relative bacteria: --------------------------------------
+heatPlotAVG <- function(group_relaAbunList, cluster_assign, monthLab){
+  
+  k <- length(unique(cluster_assign))
+  meanEst <- matrix(NA, nrow = k, ncol = 12)
+  meanSDEst <- matrix(NA, nrow = k, ncol = 12)
+  meanAct <- matrix(NA, nrow = k, ncol = 12)
+  meanSDAct <- matrix(NA, nrow = k, ncol = 12)
+  bactName <- colnames(group_relaAbunList$actualRelaGroup)
+  
+  for(i in 1:k){
+    
+    actPop <- data.frame(Cluster = cluster_assign, group_relaAbunList$actualRelaGroup) %>%
+      dplyr::filter(Cluster == i) %>%
+      dplyr::select(-Cluster)
+    meanAct[i, ] <- colMeans(actPop)
+    meanSDAct[i, ] <- apply(actPop, 2, meanSD)
+    
+    EstPop <- data.frame(Cluster = cluster_assign, group_relaAbunList$estiRelaGroup) %>%
+      dplyr::filter(Cluster == i) %>%
+      dplyr::select(-Cluster)
+    meanEst[i, ] <- colMeans(EstPop)
+    meanSDEst[i, ] <- apply(EstPop, 2, meanSD)
+    
+  }
+  
+  actTitle <- paste0(monthLab, ": Actual Average relative abundance")
+  estTitle <- paste0(monthLab, ": Estimated Average relative abundance")
+  
+  actHeat <- inner_join(t(meanAct) %>%
+                          `colnames<-`(paste0("Cluster_", 1:k)) %>%
+                          data.frame(Bact = bactName) %>%
+                          pivot_longer(!Bact, values_to = "prop_col"),
+                        t(meanSDAct) %>%
+                          `colnames<-`(paste0("Cluster_", 1:k)) %>%
+                          data.frame(Bact = bactName) %>%
+                          pivot_longer(!Bact, values_to = "prop_print")) %>%
+    ggplot(aes(x = factor(name, levels = paste0("Cluster_", 1:k), 
+                          labels = paste0("Cluster ", 1:k)), 
+               y = forcats::fct_rev(factor(Bact, levels = c("Bifidobacterium", "Other_Actinobacteria",
+                                                            "Bacteroides", "Prevotella", "Other_Bacteroidetes",
+                                                            "Faecalibacterium", "Megasphaera", "Ruminococcus", 
+                                                            "Streptococcus", "Veillonella", "Other_Firmicutes", 
+                                                            "Others"),
+                                           labels = c("Bifidobacterium", "Other Actinobacteria",
+                                                      "Bacteroides", "Prevotella", "Other Bacteroidetes",
+                                                      "Faecalibacterium", "Megasphaera", "Ruminococcus", 
+                                                      "Streptococcus", "Veillonella", "Other Firmicutes", 
+                                                      "Others"))), fill = prop_col, label = prop_print)) +
+    geom_tile() +
+    geom_text(color = "black") +
+    scale_fill_gradient(limits = c(0, 1), low = "white", high = "darkgreen") +
+    theme_minimal() +
+    theme(legend.position = "bottom") +
+    labs(x = " ", y = "Bacteria", title = actTitle, 
+         fill = "Average Relative Abundance")
+  
+  estHeat <- inner_join(t(meanEst) %>%
+                          `colnames<-`(paste0("Cluster_", 1:k)) %>%
+                          data.frame(Bact = bactName) %>%
+                          pivot_longer(!Bact, values_to = "prop_col"),
+                        t(meanSDEst) %>%
+                          `colnames<-`(paste0("Cluster_", 1:k)) %>%
+                          data.frame(Bact = bactName) %>%
+                          pivot_longer(!Bact, values_to = "prop_print")) %>%
+    ggplot(aes(x = factor(name, levels = paste0("Cluster_", 1:k), 
+                          labels = paste0("Cluster ", 1:k)), 
+               y = forcats::fct_rev(factor(Bact, levels = c("Bifidobacterium", "Other_Actinobacteria",
+                                                            "Bacteroides", "Prevotella", "Other_Bacteroidetes",
+                                                            "Faecalibacterium", "Megasphaera", "Ruminococcus", 
+                                                            "Streptococcus", "Veillonella", "Other_Firmicutes", 
+                                                            "Others"),
+                                           labels = c("Bifidobacterium", "Other Actinobacteria",
+                                                      "Bacteroides", "Prevotella", "Other Bacteroidetes",
+                                                      "Faecalibacterium", "Megasphaera", "Ruminococcus", 
+                                                      "Streptococcus", "Veillonella", "Other Firmicutes", 
+                                                      "Others"))), fill = prop_col, label = prop_print)) +
+    geom_tile() +
+    geom_text(color = "black") +
+    scale_fill_gradient(limits = c(0, 1), low = "white", high = "darkgreen") +
+    theme_minimal() +
+    theme(legend.position = "bottom") +
+    labs(x = " ", y = "Bacteria", title = estTitle, 
+         fill = "Average Relative Abundance")
+  
+  list(actHeat = actHeat, estHeat = estHeat)
+  
+}
+
+h6m <- heatPlotAVG(groupRelaTaxa[[1]], orderedClus[, 1], "6-Month")
+h8m <- heatPlotAVG(groupRelaTaxa[[2]], orderedClus[, 2], "8-Month")
+h12m <- heatPlotAVG(groupRelaTaxa[[3]], orderedClus[, 3], "12-Month")
+
+ggarrange(h6m$actHeat, h8m$actHeat, h12m$actHeat, h6m$estHeat, h8m$estHeat, h12m$estHeat,
+          ncol = 3, nrow = 2, common.legend = TRUE, legend="bottom")
 
 ### Trajectory Plot: -----------------------------------------------------------
-
 obsMovement <- function(clusAssign){
   
   ### Get the bar separating each cluster
@@ -221,10 +495,62 @@ obsMovement <- function(clusAssign){
   
 }
 
-obsMovement(salsoClus)
+obsMovement(orderedClus)
 
+### Descriptive Statistic Plot: ------------------------------------------------
+descPlot <- function(descDat_type, intVar, capt, monthLab, g1level = "yes", g2level = "no", 
+                     g1label = "Yes", g2label = "No", g1Color = "olivedrab3", g2Color = "rosybrown1"){
+  
+  k <- length(unique(descDat_type$Cluster))
+  ptitle <- paste0(monthLab, ": ", capt)
+  
+  descDat_type %>%
+    group_by(Cluster, {{intVar}}) %>%
+    summarise(n = n()) %>% 
+    mutate(freq = n / sum(n)) %>%
+    ungroup() %>%
+    ggplot(aes(x = factor(Cluster, labels = paste0("Cluster ", 1:k)), 
+               y = freq, fill = factor({{intVar}}, levels = c(g1level, g2level, "N/A"),
+                                       label = c(g1label, g2label, "Not Available")))) +
+    geom_bar(position = "stack", stat = "identity") +
+    scale_fill_manual(capt, values = c(g1Color, g2Color, "grey90")) +
+    theme_minimal() +
+    theme(legend.position = "bottom") +
+    scale_y_continuous(labels = scales::percent_format()) +
+    labs(x = " ", y = "Percentage", title = ptitle)
+  
+}
 
+plotData <- function(monthabbv, monthLabel, monthDat, monthCluster){
+  
+  descDat <- data.frame(Child_ID = obsID, monthDat[, c(1, 3, 5)], Cluster = monthCluster) %>%
+    left_join(addDat[which(addDat$Month == monthabbv), ]) %>%
+    replace(is.na(.), "N/A") %>%
+    mutate(Group = str_to_title(Group))
+  
+  plotList <- list(descPlot(descDat, Sex, "Sex", monthLabel, "M", "F", "Male", "Female", "lightblue", "lightpink"),
+                   descPlot(descDat, country, "Nationality", monthLabel, "NI", "ML", "Nicaraguan", "Malian", "darkblue", "lightgreen"),
+                   descPlot(descDat, Group, "Group", monthLabel, "Rice Bran", "Control", "Rice Bran", "Control"),
+                   descPlot(descDat, Breastfed, "Breastfed", monthLabel),
+                   descPlot(descDat, Cow_milk, "Cow milk", monthLabel),
+                   descPlot(descDat, Fruits_Natural_Juices, "Fruits Natural Juices", monthLabel),
+                   descPlot(descDat, Vegetables, "Vegetables", monthLabel),
+                   descPlot(descDat, Soups, "Soups", monthLabel),
+                   descPlot(descDat, Antibiotics, "Antibiotics", monthLabel),
+                   descPlot(descDat, Diarrhea, "Diarrhea", monthLabel),
+                   descPlot(descDat, GR, "Grains and Legumes", monthLabel),
+                   descPlot(descDat, Protein, "Protein", monthLabel))
+  
+  plotList
+  
+}
 
+plot6mo <- plotData("6MO", "6-Month", dat06, orderedClus[, 1])
+plot8mo <- plotData("8MO", "8-Month", dat08, orderedClus[, 2])
+plot12mo <- plotData("12MO", "12-Month", dat12, orderedClus[, 3])
 
+grid.arrange(grobs = plot6mo)
+grid.arrange(grobs = plot8mo)
+grid.arrange(grobs = plot12mo)
 
 ###: ---------------------------------------------------------------------------
