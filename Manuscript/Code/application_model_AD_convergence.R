2d6f644d21dd51f6eaba4ee0dbf8f2cb33156c69
Kevin Korsurat <kevin.korsurat@colostate.edu>
2024-04-10 23:12:46 -0600

    Update application_model_AD_convergence.R

--- Manuscript/Code/application_model_AD_convergence.R
+++ Manuscript/Code/application_model_AD_convergence.R
@@ -27,15 +27,13 @@ if(! file.exists(path)){
   path <- "/Users/kevinkvp/Desktop/Github Repo/ClusterZI/"
 }
 
-month_analysis <- "8m"
+month_analysis <- "6m"
 caseName <- c("One Cluster", "Singletons", "30 Clusters", "60 Clusters")
 
-r1 <- readRDS(paste0(path, "Manuscript/Result/microbiome_result_", month_analysis, "_oneClus.RData")) 
-r2 <- readRDS(paste0(path, "Manuscript/Result/microbiome_result_", month_analysis, "_singleton.RData"))
-r3 <- readRDS(paste0(path, "Manuscript/Result/microbiome_result_", month_analysis, "_init30.RData"))
-r4 <- readRDS(paste0(path, "Manuscript/Result/microbiome_result_", month_analysis, "_init60.RData"))
-
-listData <- list(r1 = r1, r2 = r2, r3 = r3, r4 = r4)
+listData <- list(r1 = readRDS(paste0(path, "Manuscript/Result/microbiome_result_", month_analysis, "_oneClus.RData")), 
+                 r2 = readRDS(paste0(path, "Manuscript/Result/microbiome_result_", month_analysis, "_singleton.RData")), 
+                 r3 = readRDS(paste0(path, "Manuscript/Result/microbiome_result_", month_analysis, "_init30.RData")), 
+                 r4 = readRDS(paste0(path, "Manuscript/Result/microbiome_result_", month_analysis, "_init60.RData")))
 
 ### Active Clusters: -----------------------------------------------------------
 lapply(1:4, function(y){sapply(1:5, function(x){apply(listData[[y]][[x]]$result$ci_result, 1, uniqueClus)}) %>%
@@ -102,272 +100,67 @@ data.frame(c1 = caseChain[allComb[, 1]], c2 = caseChain[allComb[, 2]],
   theme(axis.title.x = element_blank(), axis.title.y = element_blank()) +
   labs(title = paste0(str_extract(month_analysis, "^[:digit:]+"), "-Month: Post-MCMC Adjusted Rand Index with salso"))
 
-
-
-
 ### Acceptance Rate: -----------------------------------------------------------
-lapply(1:5, function(y){sapply(1:90, function(x){if(x > dim(r1[[y]]$result$MH_accept)[2]){return(c(NA, NA))} else{
-  indexUpdate <- r1[[y]]$result$MH_accept[, x] != -1
-  acceptVec <- r1[[y]]$result$MH_accept[indexUpdate, x]
+lapply(1:4, function(z){lapply(1:5, function(y){sapply(1:90, function(x){if(x > dim(listData[[z]][[y]]$result$MH_accept)[2]){return(c(NA, NA))} else{
+  indexUpdate <- listData[[z]][[y]]$result$MH_accept[, x] != -1
+  acceptVec <- listData[[z]][[y]]$result$MH_accept[indexUpdate, x]
   c(length(acceptVec), mean(acceptVec))}
-  }) %>% t() %>% as.data.frame() %>%
-  `colnames<-`(c("nPropose", "acceptRate")) %>%
-  mutate(Cluster = paste0("Cluster ", 1:90), case = caseName[1], chain = paste0("Chain ", y))}) 
-
-
-
-
-
-
-AcceptRate <- lapply(1:5, function(y){sapply(1:10, function(x){mean(testResult1[[y]]$result$MH_accept[which(testResult1[[y]]$result$MH_accept[, x] != -1), x])})}) %>%
-  as.data.frame() %>%
+}) %>% t() %>% as.data.frame() %>%
+    `colnames<-`(c("nPropose", "acceptRate")) %>%
+    mutate(Cluster = paste0("Cluster ", 1:90), case = caseName[z], chain = paste0("Chain ", y))}) %>%
+    bind_rows(.id = NULL)}) %>%
   bind_rows(.id = NULL) %>%
-  `rownames<-`(paste0("Cluster ", 1:10)) %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  round(3)
-
-nUpdate <- lapply(1:5, function(y){sapply(1:10, function(x){sum(testResult1[[y]]$result$MH_accept[, x] != -1)})}) %>%
-  as.data.frame() %>%
-  bind_rows(.id = NULL) %>%
-  `rownames<-`(paste0("Cluster ", 1:10)) %>%
-  `colnames<-`(paste0("Chain ", 1:5))
-
-AcceptLonger <- data.frame(AcceptRate) %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  mutate(Cluster = paste0("Cluster ", 1:10)) %>%
-  pivot_longer(!(Cluster))
-
-nLonger <- sapply(1:5, function(k){paste0(AcceptRate[, k], " (", nUpdate[, k], ")")}) %>%
-  as.data.frame() %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  mutate(Cluster = paste0("Cluster ", 1:10)) %>%
-  pivot_longer(!(Cluster))
-
-ggplot() +
-  geom_tile(data = AcceptLonger, aes(x = name, y = Cluster, fill = value)) +
-  geom_text(data = nLonger, aes(x = name, y = Cluster, label = value), color = "white") +
-  labs(x = "Chain", y = "Cluster", title = TeX("Acceptance Rate when updating $\\beta$ via Adaptive MH (One Cluster)")) +
-  theme_minimal()
-
-
-
-###: ---------------------------------------------------------------------------
-
-
-
-### Acceptance Rate for beta
-#### One Cluster
-AcceptRate <- lapply(1:5, function(y){sapply(1:10, function(x){mean(testResult1[[y]]$result$MH_accept[which(testResult1[[y]]$result$MH_accept[, x] != -1), x])})}) %>%
-  as.data.frame() %>%
-  bind_rows(.id = NULL) %>%
-  `rownames<-`(paste0("Cluster ", 1:10)) %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  round(3)
-
-nUpdate <- lapply(1:5, function(y){sapply(1:10, function(x){sum(testResult1[[y]]$result$MH_accept[, x] != -1)})}) %>%
-  as.data.frame() %>%
-  bind_rows(.id = NULL) %>%
-  `rownames<-`(paste0("Cluster ", 1:10)) %>%
-  `colnames<-`(paste0("Chain ", 1:5))
-
-AcceptLonger <- data.frame(AcceptRate) %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  mutate(Cluster = paste0("Cluster ", 1:10)) %>%
-  pivot_longer(!(Cluster))
-
-nLonger <- sapply(1:5, function(k){paste0(AcceptRate[, k], " (", nUpdate[, k], ")")}) %>%
-  as.data.frame() %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  mutate(Cluster = paste0("Cluster ", 1:10)) %>%
-  pivot_longer(!(Cluster))
-
-ggplot() +
-  geom_tile(data = AcceptLonger, aes(x = name, y = Cluster, fill = value)) +
-  geom_text(data = nLonger, aes(x = name, y = Cluster, label = value), color = "white") +
-  labs(x = "Chain", y = "Cluster", title = TeX("Acceptance Rate when updating $\\beta$ via Adaptive MH (One Cluster)")) +
-  theme_minimal()
-
-#### Singleton
-AcceptRate <- lapply(1:5, function(y){sapply(1:90, function(x){mean(testResult2[[y]]$result$MH_accept[which(testResult2[[y]]$result$MH_accept[, x] != -1), x])})}) %>%
-  as.data.frame() %>%
-  bind_rows(.id = NULL) %>%
-  `rownames<-`(paste0("Cluster ", 1:90)) %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  round(3)
-
-nUpdate <- lapply(1:5, function(y){sapply(1:90, function(x){sum(testResult2[[y]]$result$MH_accept[, x] != -1)})}) %>%
-  as.data.frame() %>%
-  bind_rows(.id = NULL) %>%
-  `rownames<-`(paste0("Cluster ", 1:90)) %>%
-  `colnames<-`(paste0("Chain ", 1:5))
-
-AcceptLonger <- data.frame(AcceptRate) %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  mutate(Cluster = paste0("Cluster ", 1:90)) %>%
-  pivot_longer(!(Cluster))
-
-nLonger <- sapply(1:5, function(k){paste0(AcceptRate[, k], " (", nUpdate[, k], ")")}) %>%
-  as.data.frame() %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  mutate(Cluster = paste0("Cluster ", 1:90)) %>%
-  pivot_longer(!(Cluster))
-
-ggplot() +
-  geom_tile(data = AcceptLonger, aes(x = name, y = Cluster, fill = value)) +
-  geom_text(data = nLonger, aes(x = name, y = Cluster, label = value), color = "white") +
-  labs(x = "Chain", y = "Cluster", title = TeX("Acceptance Rate when updating $\\beta$ via Adaptive MH (Singleton)")) +
-  theme_minimal()
-
-#### 30 Clusters
-AcceptRate <- lapply(1:5, function(y){sapply(1:30, function(x){mean(testResult3[[y]]$result$MH_accept[which(testResult3[[y]]$result$MH_accept[, x] != -1), x])})}) %>%
-  as.data.frame() %>%
-  bind_rows(.id = NULL) %>%
-  `rownames<-`(paste0("Cluster ", 1:30)) %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  round(3)
-
-nUpdate <- lapply(1:5, function(y){sapply(1:30, function(x){sum(testResult3[[y]]$result$MH_accept[, x] != -1)})}) %>%
-  as.data.frame() %>%
-  bind_rows(.id = NULL) %>%
-  `rownames<-`(paste0("Cluster ", 1:30)) %>%
-  `colnames<-`(paste0("Chain ", 1:5))
-
-AcceptLonger <- data.frame(AcceptRate) %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  mutate(Cluster = paste0("Cluster ", 1:30)) %>%
-  pivot_longer(!(Cluster))
-
-nLonger <- sapply(1:5, function(k){paste0(AcceptRate[, k], " (", nUpdate[, k], ")")}) %>%
-  as.data.frame() %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  mutate(Cluster = paste0("Cluster ", 1:30)) %>%
-  pivot_longer(!(Cluster))
-
-ggplot() +
-  geom_tile(data = AcceptLonger, aes(x = name, y = Cluster, fill = value)) +
-  geom_text(data = nLonger, aes(x = name, y = Cluster, label = value), color = "white") +
-  labs(x = "Chain", y = "Cluster", title = TeX("Acceptance Rate when updating $\\beta$ via Adaptive MH (30 Clusters)")) +
-  theme_minimal()
-
-#### 60 Clusters
-AcceptRate <- lapply(1:5, function(y){sapply(1:60, function(x){mean(testResult4[[y]]$result$MH_accept[which(testResult4[[y]]$result$MH_accept[, x] != -1), x])})}) %>%
-  as.data.frame() %>%
-  bind_rows(.id = NULL) %>%
-  `rownames<-`(paste0("Cluster ", 1:60)) %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  round(3)
-
-nUpdate <- lapply(1:5, function(y){sapply(1:60, function(x){sum(testResult4[[y]]$result$MH_accept[, x] != -1)})}) %>%
-  as.data.frame() %>%
-  bind_rows(.id = NULL) %>%
-  `rownames<-`(paste0("Cluster ", 1:60)) %>%
-  `colnames<-`(paste0("Chain ", 1:5))
-
-AcceptLonger <- data.frame(AcceptRate) %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  mutate(Cluster = paste0("Cluster ", 1:60)) %>%
-  pivot_longer(!(Cluster))
-
-nLonger <- sapply(1:5, function(k){paste0(AcceptRate[, k], " (", nUpdate[, k], ")")}) %>%
-  as.data.frame() %>%
-  `colnames<-`(paste0("Chain ", 1:5)) %>%
-  mutate(Cluster = paste0("Cluster ", 1:60)) %>%
-  pivot_longer(!(Cluster))
-
-ggplot() +
-  geom_tile(data = AcceptLonger, aes(x = name, y = Cluster, fill = value)) +
-  geom_text(data = nLonger, aes(x = name, y = Cluster, label = value), color = "white") +
-  labs(x = "Chain", y = "Cluster", title = TeX("Acceptance Rate when updating $\\beta$ via Adaptive MH (60 Clusters)")) +
-  theme_minimal()
-
-### Trace plot for beta
-lapply(1:5, function(y){sapply(1:10, function(x){sum(testResult1[[y]]$result$MH_accept[, x] != -1)})})
-
-aC <- which(sapply(1:10, function(x){sum(testResult1[[4]]$result$MH_accept[, x] != -1)}) >= 10000)
-lapply(aC, function(x){t(testResult1[[4]]$result$beta_result[x, , ]) %>%
-    as.data.frame() %>%
-    `colnames<-`(paste0("b", 1:38)) %>%
-    mutate(Iteration = 1:15000, Cluster = paste0("Cluster ", x))}) %>%
-  bind_rows(.id = NULL) %>%
-  pivot_longer(!(c(Iteration, Cluster))) %>%
-  ggplot(aes(x = Iteration, y = value, color = Cluster)) +
-  geom_line() +
-  facet_wrap(. ~ name) +
-  labs(title = TeX("Trace plot for $\\beta$ via Adpative MH (One Cluster - Chain 4)"))
-
-
-aC <- which(sapply(1:90, function(x){sum(testResult2[[1]]$result$MH_accept[, x] != -1)}) >= 10000)
-lapply(aC, function(x){t(testResult2[[1]]$result$beta_result[x, , ]) %>%
-    as.data.frame() %>%
-    `colnames<-`(paste0("b", 1:38)) %>%
-    mutate(Iteration = 1:15000, Cluster = paste0("Cluster ", x))}) %>%
-  bind_rows(.id = NULL) %>%
-  pivot_longer(!(c(Iteration, Cluster))) %>%
-  ggplot(aes(x = Iteration, y = value, color = Cluster)) +
-  geom_line() +
-  facet_wrap(. ~ name) +
-  labs(title = TeX("Trace plot for $\\beta$ via Adpative MH (Actual Singleton - Chain 1)"))
-
-### ACF plot
-# sapply(1:38, function(x){acf(as.numeric(testResult1[[1]]$result$beta_result[8, x, ]), lag.max = 100, plot = FALSE)[[1]]}) %>%
-#   `colnames<-`(paste0("b", 1:38)) %>%
-#   as.data.frame() %>%
-#   mutate(Lags = 0:100) %>%
-#   pivot_longer(!Lags) %>%
-#   ggplot(aes(x = Lags, y = value, color = name)) +
-#   geom_line() +
-#   ylim(-1, 1)
-
-### Geweke
-sapply(1:90, function(y){sapply(1:38, function(x){geweke.diag(mcmc(testResult1[[5]]$result$beta_result[y, x, ], start = 5000))$z}) %>%
-    abs() %>%
-    pnorm(lower.tail = FALSE) * 2}) %>%
-  as.data.frame() %>%
-  `colnames<-`(paste0("Cluster ", 1:90)) %>%
-  mutate(beta = paste0("b", 1:38)) %>%
-  pivot_longer(!beta) %>%
-  mutate(significant = (value < 0.05)) %>%
-  ggplot(aes(x = name, y = beta, fill = significant)) +
+  mutate(caseChain = paste0(case, " \n ", chain)) %>%
+  ggplot(aes(x = caseChain, y = factor(Cluster, levels = paste0("Cluster ", 90:1)), fill = nPropose)) +
   geom_tile() +
-  labs(title = "Geweke Statistics (Chain 5) -- MH update (Significant = not converged)")
+  geom_text(aes(label = round(acceptRate, 2)), color = "white", size = 3) +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  theme(axis.title.x = element_blank(), axis.title.y = element_blank()) +
+  labs(fill = "Number of Propose",
+       title = paste0(str_extract(month_analysis, "^[:digit:]+"), "-Month: Acceptance Rate for the cluster concentation."))
 
-### salso
-lapply(1:5, function(x){table(salso(testResult1[[x]]$result$ci_result[-(1:5000), ]))})
-lapply(1:5, function(x){data.frame(testResult1[[x]]$result$ci_result[seq(5000, 15000, 100), ])}) %>%
+### Beta Convergence: ----------------------------------------------------------
+intVar <- c(4, 8, 10, 16, 27, 36, 38)
+lapply(1:4, function(z){lapply(1:5, function(y){lapply(which(colSums(listData[[z]][[y]]$result$MH_accept != -1) >= 10000),
+                                                       function(x){data.frame(t(listData[[z]][[y]]$result$beta_result[x, intVar, ])) %>%
+                                                           `colnames<-`(paste0("b", intVar)) %>%
+                                                           mutate(iter = 1:15000, case = caseName[z], chain = paste0("Chain ", y), cluster = paste0("Cluster ", x)) %>%
+                                                           pivot_longer(!(c(iter, case, chain, cluster)))}) %>%
+    bind_rows(.id = NULL)}) %>%
+    bind_rows(.id = NULL)}) %>%
   bind_rows(.id = NULL) %>%
-  as.matrix() %>%
-  salso() %>%
-  table()
+  mutate(caseChainClus = paste0(case, " \n ", chain, " - ", cluster)) %>%
+  ggplot(aes(x = iter, y = value, color = factor(name, levels = paste0("b", intVar)))) +
+  geom_line() +
+  facet_wrap(. ~ caseChainClus) +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  guides(color = guide_legend(nrow = 1)) +
+  labs(color = "beta",
+       title = paste0(str_extract(month_analysis, "^[:digit:]+"), "-Month: Traceplot for the beta (Active Cluster only)"))
 
-lapply(1:5, function(x){table(salso(testResult2[[x]]$result$ci_result[-(1:5000), ]))})
-lapply(1:5, function(x){data.frame(testResult2[[x]]$result$ci_result[seq(5000, 15000, 100), ])}) %>%
-  bind_rows(.id = NULL) %>%
-  as.matrix() %>%
-  salso() %>%
-  table()
+### Save the result for further analysis: --------------------------------------
+burn_in <- 5000
+thin <- 100
+mcmcIter <- seq(burn_in + thin, 15000, thin)
 
-lapply(1:5, function(x){table(salso(testResult3[[x]]$result$ci_result[-(1:5000), ]))})
-lapply(1:5, function(x){data.frame(testResult3[[x]]$result$ci_result[seq(5000, 15000, 100), ])}) %>%
-  bind_rows(.id = NULL) %>%
-  as.matrix() %>%
-  salso() %>%
-  table()
+indexRun <- expand.grid(1:4, 1:5, mcmcIter)
+indexRun <- indexRun %>% arrange(Var1, Var2, Var3)
 
-lapply(1:5, function(x){table(salso(testResult4[[x]]$result$ci_result[-(1:5000), ]))})
-lapply(1:5, function(x){data.frame(testResult4[[x]]$result$ci_result[seq(5000, 15000, 100), ])}) %>%
-  bind_rows(.id = NULL) %>%
-  as.matrix() %>%
-  salso() %>%
-  table()
+resultList <- list(ci_result = lapply(1:4, function(y){lapply(1:5, function(x){listData[[y]][[x]]$result$ci_result[mcmcIter, ] %>%
+    as.data.frame()}) %>%
+    bind_rows(.id = NULL)}) %>%
+      bind_rows(.id = NULL) %>%
+      as.matrix(),
+    atrisk_result = lapply(1:2000, function(x){listData[[indexRun[x, 1]]][[indexRun[x, 2]]]$result$atrisk_result[, , indexRun[x, 3]]}) %>%
+      unlist() %>%
+      array(c(90, 38, 2000)),
+    beta_result = lapply(1:2000, function(x){listData[[indexRun[x, 1]]][[indexRun[x, 2]]]$result$beta_result[, , indexRun[x, 3]]}))
 
-###
-lapply(1:5, function(x){data.frame(testResult4[[x]]$result$ci_result[seq(5000, 15000, 100), ])}) %>%
-  bind_rows(.id = NULL) %>%
-  as.matrix() %>%
-  salso() %>%
-  as.numeric() %>%
-  table(dat06[, 1])
-
+saveRDS(resultList,
+        paste0(path, "Manuscript/Result/microbiome_result_", month_analysis, "_combined.RData"))
+###: ---------------------------------------------------------------------------
 
 
 
