3ab9aae7b6ba6105bd437530bf86383e1defad71
Kevin Korsurat <kevin.korsurat@colostate.edu>
2024-07-04 13:43:28 -0600

    Update dataset_hinton.R

--- Manuscript/Code/Application Data/dataset_hinton.R
+++ Manuscript/Code/Application Data/dataset_hinton.R
@@ -9,6 +9,11 @@ uniqueClus <- function(x){
   length(unique(x))
 }
 
+# Path
+path <- "/Users/kevinkvp/Desktop/Github Repo/ClusterZI/Manuscript/"
+resultpath <- paste0(path, "Result/selbal_HIV/")
+# file.exists(resultpath)
+
 # HIV dataset
 datHIV <- HIV
 statusHIV <- datHIV[, 62]
@@ -18,29 +23,47 @@ otuHIV <- datHIV[, 1:60]
 View(otuHIV)
 dim(otuHIV)
 
-(otuHIV/rowSums(otuHIV)) %>%
-  colMeans() %>%
-  as.numeric() %>%
-  var()
+# (otuHIV/rowSums(otuHIV)) %>%
+#   colMeans() %>%
+#   as.numeric() %>%
+#   var()
+# 
+# (otuHIV/rowSums(otuHIV)) %>%
+#   colMeans() %>%
+#   as.numeric() %>%
+#   sort(decreasing = TRUE)
+# 
+# otuHIV %>%
+#   colMeans() %>%
+#   as.numeric() %>%
+#   var()
 
-(otuHIV/rowSums(otuHIV)) %>%
-  colMeans() %>%
-  as.numeric() %>%
-  sort(decreasing = TRUE)
+set.seed(1, kind = "L'Ecuyer-CMRG")
+registerDoParallel(6)
+globalTime <- Sys.time()
+foreach(t = 1:6) %dopar% {
+  start_time <- Sys.time()
+  mod <- mod_adaptive(iter = 25000, Kmax = 10, nbeta_split = 5, 
+                      z = as.matrix(otuHIV), atrisk_init = matrix(1, nrow = 155, ncol = 60), 
+                      beta_init = matrix(0, nrow = 10, ncol = 60), 
+                      ci_init = rep(0, 155), 
+                      theta = 1, mu = 0, s2 = 1, s2_MH = 1e-3, 
+                      t_thres = 2500, launch_iter = 30, 
+                      r0g = 1, r1g = 1, r0c = 1, r1c = 1, thin = 1)
+  comp_time <- difftime(Sys.time(), start_time, units = "secs")
+  saveRDS(list(time = comp_time, mod = mod), file = paste0(resultpath, "result_selbal_HIV_chain_", t, "_init_oneClus_defaultHyper.rds"))
+}
+stopImplicitCluster()
+difftime(Sys.time(), globalTime)
 
-otuHIV %>%
-  colMeans() %>%
-  as.numeric() %>%
-  var()
-
-set.seed(1)
-result <- mod_adaptive(iter = 5000, Kmax = 10, nbeta_split = 5, 
-                       z = as.matrix(otuHIV), atrisk_init = matrix(1, nrow = 155, ncol = 60), 
-                       beta_init = matrix(0, nrow = 10, ncol = 60), 
-                       ci_init = rep(0, 155), 
-                       theta = 1, mu = 0, s2 = 1, s2_MH = 1e-3, 
-                       t_thres = 500, launch_iter = 30, 
-                       r0g = 1, r1g = 1, r0c = 1, r1c = 1, thin = 1)
+# set.seed(1)
+# result <- mod_adaptive(iter = 5000, Kmax = 10, nbeta_split = 5, 
+#                        z = as.matrix(otuHIV), atrisk_init = matrix(1, nrow = 155, ncol = 60), 
+#                        beta_init = matrix(0, nrow = 10, ncol = 60), 
+#                        ci_init = rep(0, 155), 
+#                        theta = 1, mu = 0, s2 = 1, s2_MH = 1e-3, 
+#                        t_thres = 500, launch_iter = 30, 
+#                        r0g = 1, r1g = 1, r0c = 1, r1c = 1, thin = 1)
 
 # x <- rnorm(60, 0, (10)^2)
 # exp(x)/sum(exp(x))
