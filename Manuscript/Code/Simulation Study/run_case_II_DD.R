0a5a4d9c933297d15f78ee12d4c91731c66105d0
Kevin Korsurat <kevin.korsurat@colostate.edu>
2024-07-16 12:40:29 -0600

    Create run_case_II_DD.R

---
+++ Manuscript/Code/Simulation Study/run_case_II_DD.R
@@ -0,0 +1,30 @@
+library(foreach)
+library(doParallel)
+
+### Import the data
+path <- "/Users/kevin-imac/Desktop/Github - Repo/ClusterZI/Manuscript/"
+if(! file.exists(path)){
+  path <- "/Users/kevinkvp/Desktop/Github Repo/ClusterZI/Manuscript/"
+}
+
+### Import the data
+dat <- readRDS(paste0(path, "Data/Simulation Study/simu_data_case_II.rds"))
+
+set.seed(1, kind = "L'Ecuyer-CMRG")
+registerDoParallel(3)
+result <- foreach(t = 1:20) %:%
+  foreach(k = 2:9) %dopar% {
+    
+    ### DM-DM
+    start_time <- Sys.time()
+    mod <- DMDM(iter = 2500, Kmax = k, z = as.matrix(dat[[t]]$dat),
+                beta_mat = matrix(0, nrow = k, ncol = 250), 
+                ci_init = rep(0, 50), theta = 1, thin = 1)
+    tot_time <- difftime(Sys.time(), start_time, units = "secs")
+    list(mod = mod, time = tot_time) 
+            
+}
+stopImplicitCluster()
+
+saveRDS(result, 
+        file = paste0(path, "Result/Simulation Study/simu_data_case_II_DD.rds"))
