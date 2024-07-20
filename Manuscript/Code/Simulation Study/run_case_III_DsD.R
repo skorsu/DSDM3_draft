00c664e40a33d2312fe8516bb4b6662f325d55cd
Kevin Korsurat <kevin.korsurat@colostate.edu>
2024-07-20 12:15:55 -0600

    edit DM-sDM

--- Manuscript/Code/Simulation Study/run_case_III_DsD.R
+++ Manuscript/Code/Simulation Study/run_case_III_DsD.R
@@ -12,14 +12,15 @@ dat <- readRDS(paste0(path, "Data/Simulation Study/simu_data_case_III.rds"))
 
 ### Run the model
 set.seed(1, kind = "L'Ecuyer-CMRG")
-registerDoParallel(1)
+registerDoParallel(2)
 foreach(t = 1:20) %dopar% {
   
   ### DM-DM
   start_time <- Sys.time()
   mod <- DM_DM(iter = 5000, Kmax = 10, z = as.matrix(dat[[t]]$dat),
-               beta_mat = matrix(0, nrow = 10, ncol = 100), 
-               ci_init = rep(0, 50), theta = 1e-5, thin = 1)
+               beta_init = matrix(0, nrow = 10, ncol = 100), 
+               ci_init = rep(0, 50), theta = 1e-5, mu = 0, 
+               s2 = 0.1, s2_MH = 1e-3, t_thre = 1000, thin = 1)
   tot_time <- difftime(Sys.time(), start_time, units = "secs")
   saveRDS(list(mod = mod, time = tot_time), 
           file = paste0(path, "Result/Simulation Study/simu_data_case_III_chain_", t, "_DsD.rds"))
